9caefc976e79ccfa2e38a4989506e312c5658d11
nemesis <nemesis@DELLIRIUM>
2016-09-06 17:47:38 +0200

    PieChart

--- Stan/Controllers/ApiController.cs
+++ Stan/Controllers/ApiController.cs
@@ -22,5 +22,23 @@ namespace Stan.Controllers
             return "Error: use howMany = 1 or bigger! \n" +
                 "Example: ../Api/Get?howMany=3";
         }
+
+        // GET: PieChart
+        public string PieChart() {
+
+            Dictionary<string, double> dict = new Dictionary<string, double>();
+
+            var stanovi = db.Stans.ToList();
+
+            foreach (var s in stanovi) {
+                if (dict.ContainsKey(s.Lokacija)) {
+                    ++dict[s.Lokacija];
+                } else {
+                    dict.Add(s.Lokacija, 1);
+                }
+            }
+
+            return new JavaScriptSerializer().Serialize(dict);
+        }
     }
 }
