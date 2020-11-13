b73ef3bc183455cfbfb1478a67c842769c3c8f33
Antonio Todorov <antonio.todorov13@gmail.com>
2020-11-13 15:25:56 +0100

    removed new line

--- Stan/Controllers/AccountController.cs
+++ Stan/Controllers/AccountController.cs
@@ -20,7 +20,6 @@ namespace Stan.Controllers
 
         public AccountController()
         {
-            
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
